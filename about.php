00b97d49c5500e61b6b9f9709529c4f9c8e95a04
Olugbenga Raymond <olugbengaraymond20@gmail.com>
2023-06-06 10:59:57 +0100

    Adding dummy content to about page

--- about.php
+++ about.php
@@ -1,4 +1,13 @@
 <?php
     require_once "header.html";
-    echo "<h1>About Converse</h1>";
 ?>
+<h1>About Converse</h1>
+<p>
+    lorem ;gjksf,gnasfnskgnasjk 
+    asngas gioasn asjk iosfndg 
+    asdfbiodf giksdfngponsd fg 
+    dfion fg dfiogn df gpioVero takimata eirmod 
+    takimata consetetur lorem kasd lorem erat, 
+    accusam labore et sit invidunt et clita. 
+    Gubergren rebum et magna.
+</p>
